c23b1e2a1152f68bbd8e81c809ce511325092307
tudorfis <tudor.fis@gmail.com>
2022-05-20 14:51:32 +0300

    aueqiewq

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // defining an array to work as the database (temporary solution)
 const ads = [
-  {title: 'Hello, world (again)!'}
+  {title: 'Hello, world qwewqewqe(again)!'}
 ];
 
 // adding Helmet to enhance your API's security
